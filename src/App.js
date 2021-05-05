e73f257c28f3bd853f2f8c56b9996c51264e81aa
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-05-05 09:10:45 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Page1 />
+      <Page2 />
+    </div>
+  );
+}
