f863ec3676d6b4065fa0ef41c4f16421e1907012
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-05-06 21:04:39 +0900

    23

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 export const Page1DetailA = () => {
-  const location = useLocation();
-  console.log(location);
+  const { state } = useLocation();
+  console.log(state);
   return (
     <div>
       <h1>Page1DetailAページです</h1>
